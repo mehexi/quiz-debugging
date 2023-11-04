7b4f44d229425e9b38b194c4f831d3c859e71f1e
mehedi hasan <126792834+mehexi@users.noreply.github.com>
2023-11-04 13:28:47 +0600

    all done

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ startQuiz.addEventListener("click", () => {
 const loadQuiz = async () => {
   const res = await fetch("data/quiz.json");
   const data = await res.json();
-  const quizData = data;
+  quizData = data;
   displayQuiz(quizData);
 };
 
@@ -58,19 +58,22 @@ const displayQuiz = (data) => {
     return;
   }
 
+  
+
   data.forEach((quiz, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
   </div>
 </div>`;
   });
+  
 };
 
 // EventListener for quiz submit button
@@ -108,7 +111,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
   if (storage) {
     localStorage.setItem(
       "results",
